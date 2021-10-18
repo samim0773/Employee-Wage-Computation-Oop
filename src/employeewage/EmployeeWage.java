132c3836d7e9f995591549c57c6efcfa0680e81f
Samim Aktar <samimaktar0773@gmail.com>
2021-10-18 16:56:53 +0530

    Done UC2 Calculate Daily Employee Wage

--- src/employeewage/EmployeeWage.java
+++ src/employeewage/EmployeeWage.java
@@ -1,9 +1,10 @@
 package employeewage;
 
 class EmployeeIsPresentAbsent{
-	
 	// declar private variable
 	private int isPrensent;
+	private int wagePerHour = 20;
+	private int fullDayHour = 8;
 	 
 	public void setIsPrensent(int isPrensent) {
 		// check random value not less then zero
@@ -16,35 +17,36 @@ class EmployeeIsPresentAbsent{
 	public void employeeIsPresentOrAbsent() {
 		//if random value is 1
 		if(isPrensent == 1) {
-			System.err.println("Employee is present ");
+			System.err.println("Employee is present ");	
 		}
 		//is random value is 0
 		else {
-			System.out.print("Employee is absent");
+			System.out.println("Employee is absent");
 		}
 	}
 	
+	// calculate daily employee wage
+	public void calculateDailyEmployeeWage() {
+		// calculate wage
+		int dailyWage = wagePerHour * fullDayHour;
+		// print daily wage
+		System.err.println("Daily Employee Wage : "+dailyWage);
+	}	
 }
-
 
 public class EmployeeWage {
 	public static void main(String[] args) {
 		// print welcome message
 		System.out.println("Welcome to Employee Wage Computation Program");
-		
 		// generate random number 0 1
 		int empCheck = (int) Math.floor(Math.random()* 10)  % 2;
-		
 		// create object of EmployeeIsPresentAbsent 
 		EmployeeIsPresentAbsent emp1 = new EmployeeIsPresentAbsent();
-		
 		// pass random value
 		emp1.setIsPrensent(empCheck);
-		
 		//call employeeIsPresentOrAbsent method
-		emp1.employeeIsPresentOrAbsent();		
-		
-		
+		emp1.employeeIsPresentOrAbsent();	
+		// call calculateDailyEmpWage
+		emp1.calculateDailyEmployeeWage();	
 	}
-
 }
