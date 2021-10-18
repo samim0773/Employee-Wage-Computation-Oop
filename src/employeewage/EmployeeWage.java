13adbeb6f2bc4574ca016da732cfa185f18b6661
Samim Aktar <samimaktar0773@gmail.com>
2021-10-18 19:12:37 +0530

    Done UC4 solve using switch case

--- src/employeewage/EmployeeWage.java
+++ src/employeewage/EmployeeWage.java
@@ -1,39 +1,46 @@
 package employeewage;
 
-class AddPartTimeEmployee {
-
+class SolvingUsingSwitchCase {
 	// initialize variable and assign value
-	int is_Full_Time = 1;
-	int is_Part_Time = 2;
-	int Emp_Rate_Per_Hour = 20;
-	int emp_Hour = 0;
-    public void calculateWage() {
-    	// take the random value
-    	int checkEmp = (int) Math.floor(Math.random() * 10) % 3;
-    	// check the condition emp is present or not
-    	if(checkEmp==is_Full_Time)
-    	{
-    		emp_Hour = 8;
-    		System.out.println("Employee is Full time");
-    	}
-    	// part time 2
-    	else if(checkEmp==is_Part_Time)
-    	{
-    		emp_Hour = 4;
-    		System.out.println("Employee is Part Time");
-    	}
-    	else System.out.println("Emplyee is Absent");
-    	// calculate emp wage
-    	int empWage = emp_Hour * Emp_Rate_Per_Hour;
-    	// print emp wage
-    	System.out.println("Employee Wage is : "+empWage);
-    }
+	int emp_Hour;
+	final int is_Full_Time = 1;
+	final int is_Part_Time = 2;
+	int emp_Rate_Pr_Hour = 20;
+
+	public void calculateWage() {
+		// print random number 0 1 2
+		int emp_Check = (int) Math.floor(Math.random() * 3);
+		// check condition
+		switch (emp_Check) {
+		// full time 1
+		case is_Full_Time:
+			System.out.println("Employee is Full Time");
+			emp_Hour = 8;
+			break;
+		// part time 2
+		case is_Part_Time:
+			System.out.println("Employee is Part Time ");
+			emp_Hour = 4;
+			break;
+		// default emp absent
+		default:
+			System.out.println("Emloyee is Absent");
+			emp_Hour = 0;
+		}
+		// calculate emp salary
+		int salary = emp_Rate_Pr_Hour * emp_Hour;
+		// print emp salary
+		System.out.println("The salary of employee is :" + salary);
+	}
 }
+
 public class EmployeeWage {
 	public static void main(String[] args) {
 		// print welcome message
 		System.out.println("Welcome to Employee Wage Computation Program");
-		AddPartTimeEmployee emp1 = new AddPartTimeEmployee();
+		// create object
+		SolvingUsingSwitchCase emp1 = new SolvingUsingSwitchCase();
+		// call method
 		emp1.calculateWage();
 	}
 }
